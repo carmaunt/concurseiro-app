d495bcbbb078a679e708f39bbb0ceb516e509ef9
Mauricio <carmaunt@gmail.com>
2026-05-02 22:18:58 -0300

    fix: evita crash ao cancelar login Google

--- app/src/main/java/br/com/mauricio/oconcurseiro/data/auth/GoogleAuthHelper.kt
+++ app/src/main/java/br/com/mauricio/oconcurseiro/data/auth/GoogleAuthHelper.kt
@@ -3,29 +3,37 @@ package br.com.mauricio.oconcurseiro.data.auth
 import android.content.Context
 import androidx.credentials.CredentialManager
 import androidx.credentials.GetCredentialRequest
-import com.google.android.libraries.identity.googleid.GetGoogleIdOption
+import androidx.credentials.CustomCredential
+import androidx.credentials.exceptions.NoCredentialException
+import com.google.android.libraries.identity.googleid.GetSignInWithGoogleOption
 import com.google.android.libraries.identity.googleid.GoogleIdTokenCredential
 
 suspend fun obterIdTokenGoogle(context: Context): String? {
     val credentialManager = CredentialManager.create(context)
 
-    val googleIdOption = GetGoogleIdOption.Builder()
-        .setFilterByAuthorizedAccounts(false)
-        .setServerClientId("389724825283-44h5hhvdp7ion9ul7srmllej47p0llg8.apps.googleusercontent.com")
-        .build()
+    val googleOption = GetSignInWithGoogleOption.Builder(
+        "389724825283-44h5hhvdp7ion9ul7srmllej47p0llg8.apps.googleusercontent.com"
+    ).build()
 
     val request = GetCredentialRequest.Builder()
-        .addCredentialOption(googleIdOption)
+        .addCredentialOption(googleOption)
         .build()
 
-    val result = credentialManager.getCredential(
-        request = request,
-        context = context
-    )
+    val result = try {
+        credentialManager.getCredential(
+            request = request,
+            context = context
+        )
+    } catch (_: NoCredentialException) {
+        return null
+    } catch (_: Exception) {
+        return null
+    }
 
     val credential = result.credential
 
-    if (credential is androidx.credentials.CustomCredential &&
+    if (
+        credential is CustomCredential &&
         credential.type == GoogleIdTokenCredential.TYPE_GOOGLE_ID_TOKEN_CREDENTIAL
     ) {
         val googleCredential = GoogleIdTokenCredential.createFrom(credential.data)
