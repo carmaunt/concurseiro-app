d1ed8568c75614bf930fb94fb7a48a79b3b466ba
carmaunt <83962354+carmaunt@users.noreply.github.com>
2026-05-03 08:39:51 -0300

    feat(domain): add FiltroParams domain model

---
+++ domain/src/main/java/br/com/mauricio/oconcurseiro/domain/model/FiltroParams.kt
@@ -0,0 +1,17 @@
+package br.com.mauricio.oconcurseiro.domain.model
+
+data class FiltroParams(
+    val texto: String? = null,
+    val disciplina: String? = null,
+    val disciplinaId: Long? = null,
+    val assunto: String? = null,
+    val assuntoId: Long? = null,
+    val banca: String? = null,
+    val bancaId: Long? = null,
+    val instituicao: String? = null,
+    val instituicaoId: Long? = null,
+    val ano: Int? = null,
+    val cargo: String? = null,
+    val nivel: String? = null,
+    val modalidade: String? = null
+)
