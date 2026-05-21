bb005e613d997af60934035cbbb15ad6bd915812
carmaunt <83962354+carmaunt@users.noreply.github.com>
2026-05-20 21:51:03 -0300

    feat: add privacy and data screen

---
+++ app/src/main/java/br/com/mauricio/oconcurseiro/ui/screens/privacidade/PrivacyConstants.kt
@@ -0,0 +1,3 @@
+package br.com.mauricio.oconcurseiro.ui.screens.privacidade
+
+const val PRIVACY_POLICY_URL = "https://carmaunt.github.io/politica-privacidade-oconcurseiro/pagina-politica/politica-de-privacidade.html"
