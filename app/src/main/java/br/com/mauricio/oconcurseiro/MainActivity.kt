4f6fa2a710061ceece025ab7314c2f0db52e5221
carmaunt <83962354+carmaunt@users.noreply.github.com>
2026-06-19 11:49:33 -0300

    feat: track app opened event

--- app/src/main/java/br/com/mauricio/oconcurseiro/MainActivity.kt
+++ app/src/main/java/br/com/mauricio/oconcurseiro/MainActivity.kt
@@ -7,17 +7,27 @@ import androidx.activity.enableEdgeToEdge
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import androidx.lifecycle.lifecycleScope
+import br.com.mauricio.oconcurseiro.data.analytics.AnalyticsRepository
 import br.com.mauricio.oconcurseiro.ui.navigation.AppNavigation
 import br.com.mauricio.oconcurseiro.ui.theme.OConcurseiroTheme
-
 import dagger.hilt.android.AndroidEntryPoint
+import javax.inject.Inject
+import kotlinx.coroutines.launch
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
+
+    @Inject lateinit var analyticsRepository: AnalyticsRepository
+
     override fun onCreate(savedInstanceState: Bundle?) {
         val splashScreen = installSplashScreen()
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
+
+        lifecycleScope.launch {
+            analyticsRepository.trackAppOpened()
+        }
 
         setContent {
             OConcurseiroTheme {
